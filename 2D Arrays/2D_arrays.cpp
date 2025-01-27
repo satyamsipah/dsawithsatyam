95c47f6a3402d30f9f87c33adf3ed72f3325d847
Satyam Maddheshiya <127894454+satyamsipah@users.noreply.github.com>
2025-01-28 02:17:17 +0530

    Create 2D_arrays.cpp

---
+++ 2D Arrays/2D_arrays.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int students[3][3] = {{100,100,100}, {85,74,89}, {63,72,65}};
+    cout << students[0][0] << endl;
+    cout << students[0][1] << endl;
+    cout << students[1][0] << endl;
+    cout << students[1][2] << endl;
+    cout << students[2][2] << endl;
+
+    return 0;
+}
