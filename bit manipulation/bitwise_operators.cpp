cc6f8f1e17031dbd68ad952751afff7e1b9bba2d
Satyam Maddheshiya <127894454+satyamsipah@users.noreply.github.com>
2025-02-25 00:51:03 +0530

    Create bitwise_operators.cpp

---
+++ bit manipulation/bitwise_operators.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << (3 & 5) << endl; // bitwise AND
+    cout << (3 | 5) << endl; // bitwise OR
+    cout << (3 ^ 5) << endl; // bitwise XOR
+
+    return 0;
+}
