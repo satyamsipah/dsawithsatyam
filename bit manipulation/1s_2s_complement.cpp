e0a705483b39b981e24152512ed2c9bd2f476fa8
Satyam Maddheshiya <127894454+satyamsipah@users.noreply.github.com>
2025-02-25 00:52:05 +0530

    bit manipulation

---
+++ bit manipulation/1s_2s_complement.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << (~0) << endl;
+    cout << (~1) << endl; 
+    cout << (~-1) << endl;
+    cout << (~6) << endl;
+    cout << (~-26) << endl;
+
+    return 0;
+}
---
+++ bit manipulation/binary_shift_operators.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << (7 << 2) << endl; // left shift operators
+    cout << (7 >> 2) << endl; // right shift operators
+
+    return 0;
+}
